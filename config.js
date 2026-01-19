891884dc2d422748f8a5c434c7733c254862ae14
kiambuman6 <157136122+kiambuman6@users.noreply.github.com>
2026-01-19 10:06:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const ownerNumber = process.env.OWNER_NUMBER || "254793859108"; // fallback if not set in .env
+export const ownerNumber = process.env.OWNER_NUMBER || "97430752401"; // fallback if not set in .env
 export const caption = "🔥 Powered by Ernest Tech House"; // just in case you forgot this too
-export const ownerNumbers = "254793859108"; // Without @s.whatsapp.net
+export const ownerNumbers = "97430752401"; // Without @s.whatsapp.net
